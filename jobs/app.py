3391e4597ae64826238f62dde5141d3609ea719c
Anthony Dominguez <adominguez15722@gmail.com>
2022-03-14 11:28:06 -0500

    testing open_connection function

--- jobs/app.py
+++ jobs/app.py
@@ -6,6 +6,11 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 
+def open_connection():
+    getattr(g, '_connection')
+    return getattr('_connection')
+
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():
