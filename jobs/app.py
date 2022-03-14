bd2b87010836b8ccdcc56037ae0b36cc116aa6e8
Anthony Dominguez <adominguez15722@gmail.com>
2022-03-14 16:48:52 -0500

    adding employer reviews pt6

--- jobs/app.py
+++ jobs/app.py
@@ -72,6 +72,7 @@ def review(employer_id):
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)'
         (review, rating, title, date, status, employer_id)
         commit=True)
+        
         return redirect(url_for('employer.html', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
 
