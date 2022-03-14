76445651d0a727664aba945a68ebbf69ee6adc75
Anthony Dominguez <adominguez15722@gmail.com>
2022-03-14 16:14:36 -0500

    display individual employers pt10

--- jobs/app.py
+++ jobs/app.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
-    return render_template('employer.html', employer=employer, jobs=jobs)
+    
+    reviews = execute_sql('SELECT * FROM reviews WHERE id=?')
+    execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
 
 @app.route('/job/<job_id>')
