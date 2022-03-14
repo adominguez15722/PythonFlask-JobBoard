1c4245821c12700c9c670730bc9f4aa9f01580bf
Anthony Dominguez <adominguez15722@gmail.com>
2022-03-14 16:39:34 -0500

    adding employer reviews pt3

--- jobs/app.py
+++ jobs/app.py
@@ -63,6 +63,12 @@ def employer(employer_id):
 
 @app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
     return render_template('review.html', employer_id=employer_id)
 
 
